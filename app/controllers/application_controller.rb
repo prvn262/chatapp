ce6a3125aceb4bd93df6bb26740cc5d7eb27fc6d
prvn262 <prvn262@gmail.com>
2025-10-03 15:08:03 +0530

    unusuall

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  afsfsdfsfsf
+  asdf
+
+
+  asfdsaf
+  asdfdas
+  sdfsaf
+
+  asdf
+
 end
